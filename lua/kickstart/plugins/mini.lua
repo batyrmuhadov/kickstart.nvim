a8c665b275e48e7dc3acd32cbc7f62f853848ac0
Batyr <83823943+batyrmuhadov@users.noreply.github.com>
2024-10-06 04:19:01 +0500

    Update mini.lua

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -96,4 +96,23 @@ return { -- Collection of various small independent pluins/modules
 
     require('mini.icons').setup {}
   end,
+  { -- icons
+    'echasnovski/mini.icons',
+    lazy = true,
+    opts = {
+      file = {
+        ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
+        ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+      },
+      filetype = {
+        dotenv = { glyph = '', hl = 'MiniIconsYellow' },
+      },
+    },
+    init = function()
+      package.preload['nvim-web-devicons'] = function()
+        require('mini.icons').mock_nvim_web_devicons()
+        return package.loaded['nvim-web-devicons']
+      end
+    end,
+  },
 }
